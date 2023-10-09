0f50bbeca997f89f3be27f634f2d194782625f4c
IPowerW <Leon@wenderoth.de>
2023-10-10 01:35:39 +0200

    echo test

--- android/app/src/main/java/com/farsight/cda/blockguard/MainActivity.java
+++ android/app/src/main/java/com/farsight/cda/blockguard/MainActivity.java
@@ -1,5 +1,13 @@
 package com.farsight.cda.blockguard;
 
+import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        registerPlugin(com.example.myapp.EchoPlugin.class);
+        super.onCreate(savedInstanceState);
+    }
+}
